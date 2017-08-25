ac7e67fa98422e2c0d7025f00ef50fbcc2bb505a
hearticy888 <hearticy@126.com>
2017-08-25 14:44:13 +0800

    Update demo helper.

--- src/EDUGraphAPI.Web/Models/DemoPage.cs
+++ src/EDUGraphAPI.Web/Models/DemoPage.cs
@@ -14,23 +14,34 @@ namespace EDUGraphAPI.Web.Models
 
         public DemoPage()
         {
-            this.Links = new HashSet<Link>();
+            this.Functions = new HashSet<Functions>();
         }
 
-        public ISet<Link> Links { get; set; }
+        public ISet<Functions> Functions { get; set; }
     }
 
-    public class Link
+    public class Functions
     {
-        public Link()
+        public Functions()
+        {
+            this.Files = new HashSet<Files>();
+        }
+        public string Title { get; set; }
+        public string Tab { get; set; }
+        public ISet<Files> Files { get; set; }
+    }
+
+    public class Files
+    {
+        public Files()
         {
             this.Methods = new HashSet<Method>();
         }
-        public string Title { get; set; }
-
         public string Url { get; set; }
 
-        public ISet <Method> Methods { get; set; }
+
+
+        public ISet<Method> Methods { get; set; }
     }
 
     public class Method
